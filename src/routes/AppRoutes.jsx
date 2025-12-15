fbe317fde267a3d7b30a0a6f3361ce6579cef43e
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2025-12-15 16:35:04 +0000

    Rotas do dashboard adicionadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -8,7 +8,10 @@ import DashboardAdmin from "../pages/admin/DashboardAdmin";
 import AgendamentosAdmin from "../pages/admin/AgendamentosAdmin";
 import ServicosAdmin from "../pages/admin/ServicosAdmin";
 import CategoriasAdmin from "../pages/admin/CategoriasAdmin";
-
+import ProdutosAdmin from "../pages/admin/ProdutosAdmin";
+import EncomendasAdmin from "../pages/admin/EncomendasAdmin";
+import EquipeAdmin from "../pages/admin/EquipeAdmin";
+import NotFoundAdmin from "../pages/admin/NotFoundAdmin";
 
 export default function AppRoutes () {
     return (
@@ -17,12 +20,18 @@ export default function AppRoutes () {
             <Route path="/" element={<Home />} />
             <Route path="*" element={<NotFound />} />
 
-            {/*Rotas do super admin */}
+            {/*Rotas de autenticação */}
+
+            {/*Rotas do admin */}
             <Route path="/dashboard/admin/">
                 <Route path="" element={<DashboardAdmin />} />
                 <Route path="agendamentos" element={<AgendamentosAdmin />} />
                 <Route path="servicos" element={<ServicosAdmin />} />
                 <Route path="categorias" element={<CategoriasAdmin />} />
+                <Route path="produtos" element={<ProdutosAdmin />} />
+                <Route path="encomendas" element={<EncomendasAdmin />} />
+                <Route path="equipe" element={<EquipeAdmin />} />
+                <Route path="*" element={<NotFoundAdmin />} />
             </Route>
         </Routes>
     )
