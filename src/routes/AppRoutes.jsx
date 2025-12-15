ce56c0a41adfd01edcdd227ba471c3ca5ac2440a
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2025-12-15 17:10:15 +0000

    Rotas de login adicionadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/site/Home";
 import NotFound from "../pages/site/NotFound";
 
+//Autenticação
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import ForgotPassword from "../pages/auth/ForgotPassword";
+
 //Admin
 import DashboardAdmin from "../pages/admin/DashboardAdmin";
 import AgendamentosAdmin from "../pages/admin/AgendamentosAdmin";
@@ -21,6 +26,9 @@ export default function AppRoutes () {
             <Route path="*" element={<NotFound />} />
 
             {/*Rotas de autenticação */}
+            <Route path="/auth/login" element={<Login />} />
+            <Route path="/auth/cadastrar-se" element={<Register />} />
+            <Route path="/auth/esqueci-senha" element={<ForgotPassword />} />
 
             {/*Rotas do admin */}
             <Route path="/dashboard/admin/">
