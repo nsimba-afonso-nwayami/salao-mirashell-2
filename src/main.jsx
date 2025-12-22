6b82a916dc20700a59943d24feb417dc6f0bf340
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-12-22 09:45:57 +0100

    Componente para carregar as páginas no topo adicionado

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { Toaster } from "react-hot-toast";
+import ScrollToTop from "./components/ScrollToTop";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop /> 
       <Toaster position="top-right" reverseOrder={false} />
       <AppRoutes />
     </BrowserRouter>
