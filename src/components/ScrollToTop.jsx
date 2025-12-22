688367433fca900df9933d9476ea277a58bf6799
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2025-12-22 10:14:34 +0100

    Componente Scroll atualizado

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+/*import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
@@ -14,4 +14,29 @@ const ScrollToTop = () => {
   return null;
 };
 
+export default ScrollToTop;*/
+
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTop = () => {
+  const { pathname, hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      // Se houver hash, rola para o elemento específico
+      const element = document.querySelector(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    } else {
+      // Se não houver hash, rola para o topo
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [pathname, hash]);
+
+  return null;
+};
+
 export default ScrollToTop;
+
