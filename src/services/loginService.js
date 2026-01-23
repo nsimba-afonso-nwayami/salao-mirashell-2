a6bda9054ece5cbf1a445672448161194ae2ac55
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-23 14:36:07 +0100

    Service do login feito

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,1 +1,23 @@
-import { api } from "./api";
+export async function loginUser({ username, senha }) {
+  const response = await fetch("https://api2.nwayami.com/api/jwt-logar/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      password: senha,
+    }),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.detail || "Credenciais inválidas");
+  }
+
+  localStorage.setItem("accessToken", data.access);
+  localStorage.setItem("refreshToken", data.refresh);
+
+  return data;
+}
