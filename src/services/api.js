b8bbdcc19b2af22c663a67683955b8910835029b
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-23 15:38:26 +0100

    Caminho da API corrigida

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://traga-rapido.fimbatec.com/api/",
+  baseURL: "https://api2.nwayami.com/api/",
+});
+
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
