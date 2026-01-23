ab6746c8200b376dd9c4d90fa7f90ba81c5b810e
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-23 14:35:12 +0100

    Rotas do admin privadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+
 //Site
 import Home from "../pages/site/Home";
 import Agendar from "../pages/site/Agendar";
@@ -25,36 +27,39 @@ import EncomendasAdmin from "../pages/admin/EncomendasAdmin";
 import EquipeAdmin from "../pages/admin/EquipeAdmin";
 import NotFoundAdmin from "../pages/admin/NotFoundAdmin";
 
-export default function AppRoutes () {
-    return (
-        <Routes>
-            {/*Rotas do site */}
-            <Route path="/" element={<Home />} />
-            <Route path="/agendar" element={<Agendar />} />
-            <Route path="/loja" element={<Loja />} />
-            <Route path="/loja/encomendar" element={<Encomendar />} />
-            <Route path="/servico/cabeleireiro" element={<ServicoCabeleireiro />} />
-            <Route path="/servico/perucas" element={<ServicoPerucas />} />
-            <Route path="/servico/trancas" element={<ServicoTrancas />} />
-            <Route path="/servico/estetica" element={<ServicoEstetica />} />
-            <Route path="*" element={<NotFound />} />
+export default function AppRoutes() {
+  return (
+    <Routes>
+      {/*Rotas do site */}
+      <Route path="/" element={<Home />} />
+      <Route path="/agendar" element={<Agendar />} />
+      <Route path="/loja" element={<Loja />} />
+      <Route path="/loja/encomendar" element={<Encomendar />} />
+      <Route path="/servico/cabeleireiro" element={<ServicoCabeleireiro />} />
+      <Route path="/servico/perucas" element={<ServicoPerucas />} />
+      <Route path="/servico/trancas" element={<ServicoTrancas />} />
+      <Route path="/servico/estetica" element={<ServicoEstetica />} />
+      <Route path="*" element={<NotFound />} />
 
-            {/*Rotas de autenticação */}
-            <Route path="/auth/login" element={<Login />} />
-            <Route path="/auth/cadastrar-se" element={<Register />} />
-            <Route path="/auth/esqueci-senha" element={<ForgotPassword />} />
+      {/*Rotas de autenticação */}
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/cadastrar-se" element={<Register />} />
+      <Route path="/auth/esqueci-senha" element={<ForgotPassword />} />
 
-            {/*Rotas do admin */}
-            <Route path="/dashboard/admin">
-                <Route path="" element={<DashboardAdmin />} />
-                <Route path="agendamentos" element={<AgendamentosAdmin />} />
-                <Route path="servicos" element={<ServicosAdmin />} />
-                <Route path="categorias" element={<CategoriasAdmin />} />
-                <Route path="produtos" element={<ProdutosAdmin />} />
-                <Route path="encomendas" element={<EncomendasAdmin />} />
-                <Route path="equipe" element={<EquipeAdmin />} />
-                <Route path="*" element={<NotFoundAdmin />} />
-            </Route>
-        </Routes>
-    )
+      {/*Rotas do admin */}
+      {/* Rotas do admin (PROTEGIDAS) */}
+      <Route element={<PrivateRoute />}>
+        <Route path="/dashboard/admin">
+          <Route index element={<DashboardAdmin />} />
+          <Route path="agendamentos" element={<AgendamentosAdmin />} />
+          <Route path="servicos" element={<ServicosAdmin />} />
+          <Route path="categorias" element={<CategoriasAdmin />} />
+          <Route path="produtos" element={<ProdutosAdmin />} />
+          <Route path="encomendas" element={<EncomendasAdmin />} />
+          <Route path="equipe" element={<EquipeAdmin />} />
+          <Route path="*" element={<NotFoundAdmin />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
