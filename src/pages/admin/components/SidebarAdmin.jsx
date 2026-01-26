5871a42ac135dcf9aac94624fc3c51d566ca4808
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-01-26 11:27:32 +0100

    Galeria adicionada noa navegação

--- src/pages/admin/components/SidebarAdmin.jsx
+++ src/pages/admin/components/SidebarAdmin.jsx
@@ -85,6 +85,13 @@ export default function SidebarAdmin({ sidebarOpen, setSidebarOpen }) {
               Encomendas
             </Link>
             <Link
+              to="/dashboard/admin/galeria"
+              className="block p-3 rounded-lg hover:bg-stone-50 cursor-pointer text-stone-600 hover:text-[#A2672D] transition-colors"
+            >
+              <i className="fas fa-image mr-3 w-5 text-[#A2672D]"></i>{" "}
+              Galeria
+            </Link>
+            <Link
               to="/dashboard/admin/equipe"
               className="block p-3 rounded-lg hover:bg-stone-50 cursor-pointer text-stone-600 hover:text-[#A2672D] transition-colors"
             >
